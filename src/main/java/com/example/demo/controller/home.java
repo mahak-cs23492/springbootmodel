8ece45ec58199e9dc6d8f8adab69677d9a865ac7
Mahak Gupta <mahakg2588@gmail.com>
2026-01-19 15:19:21 +0530

    five students data added

--- src/main/java/com/example/demo/controller/home.java
+++ src/main/java/com/example/demo/controller/home.java
@@ -3,13 +3,22 @@ import com.example.demo.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 public class home {
     @GetMapping("/about")
-    public StudentModel getStudent(){
-        StudentModel student=new StudentModel(1,"Mahak","mahakg2005@gmail.com");
-        return student;
+    public ArrayList<StudentModel> getStudent() {
+        StudentModel student = new StudentModel(1, "Mahak", "mahakg2005@gmail.com");
 
+        ArrayList<StudentModel> list = new ArrayList<>();
+        list.add(new StudentModel(1, "Mahak", "sajko"));
+        list.add(new StudentModel(2, "Shagun", "qwsb"));
+        list.add(new StudentModel(3, "Suhani", "njsb"));
+        list.add(new StudentModel(4, "Shreya", "bsjk"));
+        list.add(new StudentModel(5, "Riya", "swvd"));
+        return list;
     }
+
 
 }
