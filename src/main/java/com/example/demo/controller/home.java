66d6d51f791bb6858d3512132da5250e7e364f73
Mahak Gupta <mahakg2588@gmail.com>
2026-01-19 15:09:00 +0530

    add api

--- src/main/java/com/example/demo/controller/home.java
+++ src/main/java/com/example/demo/controller/home.java
@@ -1,13 +1,15 @@
 package com.example.demo.controller;
-
+import com.example.demo.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class home {
     @GetMapping("/about")
-    public String home(){
-        return "This is the about page";
+    public StudentModel getStudent(){
+        StudentModel student=new StudentModel(1,"Mahak","mahakg2005@gmail.com");
+        return student;
+
     }
 
 }
